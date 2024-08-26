49751afbe6eb104cd0b2ae40e59288831bd0fd96
Johan Irving Sølotft <100198311+Johansoltoft@users.noreply.github.com>
2024-08-27 00:33:52 +0200

    Update create_network.py

--- create_network.py
+++ create_network.py
@@ -2,9 +2,6 @@ import pandas as pd
 import networkx as nx
 from game.network import Network
 
-G = 'assets/network/fully_connected_15_nodes.gexf'
-df = pd.read_csv('assets/network/TestNetwork.csv')
-
 def load_gexf_to_network(gexf_file_path, network):
     # Load the GEXF file
     graph = nx.read_gexf(gexf_file_path)
@@ -28,8 +25,12 @@ def load_gexf_to_network(gexf_file_path, network):
         node2 = node_dict[target]
         network.add_edge(node1, node2)
 
-# Usage
-network = Network()
-load_gexf_to_network(G, network)
-
-
+def create_network():
+    # Initialize the network
+    network = Network()
+    
+    # Load data into the network from the GEXF file
+    G = 'assets/network/fully_connected_15_nodes.gexf'
+    load_gexf_to_network(G, network)
+    
+    return network
