3f597b19361236b469491ec8bd545978368c8871
Johan Irving Sølotft <100198311+Johansoltoft@users.noreply.github.com>
2024-08-26 20:35:16 +0200

    Update edge.py

---
+++ game/edge.py
@@ -0,0 +1,7 @@
+class Edge:
+    def __init__(self, node1, node2):
+        self.node1 = node1
+        self.node2 = node2
+
+    def draw(self, screen, color):
+        pygame.draw.line(screen, color, (self.node1.x, self.node1.y), (self.node2.x, self.node2.y), 2)
