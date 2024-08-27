e6b62f3443266026b2d5780b2eae90e3bc6d21eb
Johan Irving Sølotft <100198311+Johansoltoft@users.noreply.github.com>
2024-08-27 23:49:16 +0200

    Update network.py

--- game/network.py
+++ game/network.py
@@ -16,14 +16,13 @@ class Network:
         edge = Edge(node1, node2)
         self.edges.append(edge)
 
+    
     def draw(self, screen, color, camera):
-        # Draw edges
         for edge in self.edges:
             start_pos = camera.apply(pygame.Rect(edge.node1.x, edge.node1.y, 0, 0)).topleft
             end_pos = camera.apply(pygame.Rect(edge.node2.x, edge.node2.y, 0, 0)).topleft
             pygame.draw.line(screen, color, start_pos, end_pos, 2)
 
-        # Draw nodes as simple rectangles or circles
         for node in self.nodes:
-            node_rect = pygame.Rect(node.x, node.y, node.size, node.size)
-            pygame.draw.rect(screen, color, camera.apply(node_rect))
+            node_center = camera.apply(pygame.Rect(node.x, node.y, 0, 0)).center
+            pygame.draw.circle(screen, color, node_center, node.size)
