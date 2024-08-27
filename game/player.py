487614cec81a5f8383c291eede35a7c563c60faf
Johan Irving Sølotft <100198311+Johansoltoft@users.noreply.github.com>
2024-08-27 21:10:35 +0200

    Update player.py

--- game/player.py
+++ game/player.py
@@ -23,51 +23,52 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, keys):
         if not self.moving:
-            # Check for diagonal movement options
+            target_node = None
+            
+            # Check for diagonal movement
             if keys[pygame.K_UP] and keys[pygame.K_RIGHT]:
                 if 'up-right' in self.node.neighbors:
-                    self.move_to_node(self.node.neighbors['up-right'])
+                    target_node = self.node.neighbors['up-right']
                 elif 'up' in self.node.neighbors and 'right' in self.node.neighbors:
-                    # If there is no direct 'up-right' option, you could potentially offer other options
-                    # For example, you could move up or right based on a condition, or just move up-right:
-                    self.move_to_node(self.node.neighbors['up-right'])
-            
+                    target_node = self.node.neighbors['up']
             elif keys[pygame.K_UP] and keys[pygame.K_LEFT]:
                 if 'up-left' in self.node.neighbors:
-                    self.move_to_node(self.node.neighbors['up-left'])
+                    target_node = self.node.neighbors['up-left']
                 elif 'up' in self.node.neighbors and 'left' in self.node.neighbors:
-                    self.move_to_node(self.node.neighbors['up-left'])
-            
+                    target_node = self.node.neighbors['up']
             elif keys[pygame.K_DOWN] and keys[pygame.K_RIGHT]:
                 if 'down-right' in self.node.neighbors:
-                    self.move_to_node(self.node.neighbors['down-right'])
+                    target_node = self.node.neighbors['down-right']
                 elif 'down' in self.node.neighbors and 'right' in self.node.neighbors:
-                    self.move_to_node(self.node.neighbors['down-right'])
-    
+                    target_node = self.node.neighbors['down']
             elif keys[pygame.K_DOWN] and keys[pygame.K_LEFT]:
                 if 'down-left' in self.node.neighbors:
-                    self.move_to_node(self.node.neighbors['down-left'])
+                    target_node = self.node.neighbors['down-left']
                 elif 'down' in self.node.neighbors and 'left' in self.node.neighbors:
-                    self.move_to_node(self.node.neighbors['down-left'])
-    
-            # Check for single direction movement options
+                    target_node = self.node.neighbors['down']
+            # Check for single direction movement
             elif keys[pygame.K_RIGHT] and 'right' in self.node.neighbors:
-                self.move_to_node(self.node.neighbors['right'])
+                target_node = self.node.neighbors['right']
             elif keys[pygame.K_LEFT] and 'left' in self.node.neighbors:
-                self.move_to_node(self.node.neighbors['left'])
+                target_node = self.node.neighbors['left']
             elif keys[pygame.K_UP] and 'up' in self.node.neighbors:
-                self.move_to_node(self.node.neighbors['up'])
+                target_node = self.node.neighbors['up']
             elif keys[pygame.K_DOWN] and 'down' in self.node.neighbors:
-                self.move_to_node(self.node.neighbors['down'])
+                target_node = self.node.neighbors['down']
+
+            # Move to the determined target node, if any
+            if target_node:
+                self.move_to_node(target_node)
         else:
+            # Update the player's position
             self.rect.x += self.direction.x
             self.rect.y += self.direction.y
-            # Check if the player has reached the target node
             if self.direction.length() > 0 and self.direction.dot(pygame.math.Vector2(self.target_node.x - self.rect.x, self.target_node.y - self.rect.y)) <= 0:
                 self.rect.center = (self.target_node.x, self.target_node.y)
                 self.node = self.target_node
                 self.message = self.node.message
                 self.moving = False
+
 
     def draw_message(self, surface, camera_offset):
         if self.message:
